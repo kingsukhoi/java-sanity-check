6a46b81d77e1edad410a57953197278030442dff
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2024-01-25 18:55:03 -0700

    Add health check and header info

--- src/main/java/ca/farsos/sanitycheck/Routes.java
+++ src/main/java/ca/farsos/sanitycheck/Routes.java
@@ -4,17 +4,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class Routes {
     Logger logger
             = LoggerFactory.getLogger(Routes.class);
 
+    @GetMapping("/")
+    public String health(@RequestHeader Map<String, String> headers) {
+        for (String key : headers.keySet()) {
+            logger.info(String.format("%s: %s", key, headers.get(key)));
+        }
+        return "Running";
+    }
+
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s", name);
+    }
+
+    @GetMapping("/headerinfo")
+    public Map<String, String> dumpHeaders(@RequestHeader Map<String, String> headers) {
+        return headers;
     }
 
     @GetMapping("/die")
