ffdc5d505240559b4d1740965924d072f5d5381f
Farsos Bulsara <16597052+kingsukhoi@users.noreply.github.com>
2025-01-17 12:02:11 -0700

    Escape user input in responses for added security.
    Implemented HTML escaping for user-provided inputs to prevent potential security vulnerabilities, such as XSS. This includes escaping "name" parameter in `/hello` and header values in `/headerinfo` endpoints. Minor formatting adjustments were also made for consistency.

--- src/main/java/ca/farsos/sanitycheck/Routes.java
+++ src/main/java/ca/farsos/sanitycheck/Routes.java
@@ -2,42 +2,47 @@ package ca.farsos.sanitycheck;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.Map;
 
 @RestController
 public class Routes {
-    Logger logger
-            = LoggerFactory.getLogger(Routes.class);
+  Logger logger
+          = LoggerFactory.getLogger(Routes.class);
 
-    @GetMapping("/")
-    public String health(@RequestHeader Map<String, String> headers) {
-        for (String key : headers.keySet()) {
-            logger.info(String.format("%s: %s", key, headers.get(key)));
-        }
-        return "Running";
+  @GetMapping("/")
+  public String health(@RequestHeader Map<String, String> headers) {
+    for (String key : headers.keySet()) {
+      logger.info("{}: {}", key, headers.get(key));
     }
+    return "Running";
+  }
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s", name);
-    }
+  @GetMapping("/hello")
+  public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+    return String.format("Hello %s", HtmlUtils.htmlEscape(name, "UTF-8"));
+  }
 
-    @GetMapping("/headerinfo")
-    public Map<String, String> dumpHeaders(@RequestHeader Map<String, String> headers) {
-        return headers;
+  @GetMapping("/headerinfo")
+  public Map<String, String> dumpHeaders(@RequestHeader Map<String, String> headers) {
+    for (String key : headers.keySet()) {
+      var curr = headers.get(key);
+      curr = HtmlUtils.htmlEscape(curr, "UTF-8");
+      headers.put(key, curr);
     }
+    return headers;
+  }
 
-    @GetMapping("/die")
-    public String die() {
-        logger.error("Received kill signal. Good by cruel world!!!!");
-        System.exit(1);
-        return "If you are seeing me, I'm not dead";
-    }
+  @GetMapping("/die")
+  public String die() {
+    logger.error("Received kill signal. Good by cruel world!!!!");
+    System.exit(1);
+    return "If you are seeing me, I'm not dead";
+  }
 
 }
